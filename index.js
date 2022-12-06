f87cee2816ac963ee0af5334f8cdccb48ff1bd9a
shashikant88093 <shashi94goswami@gmail.com>
2022-12-06 23:07:21 +0530

    todo ejs add complete

--- index.js
+++ index.js
@@ -3,47 +3,23 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+let lists = ['Buy Food','Cook Food','Eat Food']
 app.get("/", (req, res) => {
   let today = new Date();
-  let currentDay = today.getDay();
-  var day = "";
-
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tueday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error : current day is equal to : " + currentDay)
+  let options = {
+    weekday :"long",
+    day:"numeric",
+    month:"long",
+    year:"numeric"
   }
-  // if (currentDay === 6 || currentDay === 0) {
-    // res.sendFile(__dirname + "/weekend.html");
-  // } else {
-    // res.sendFile(__dirname + "/weekday.html");
-    // day = "Weekday"
-
-  // }
-  res.render("list",{kindOfDay:day})
-
+  var day = today.toLocaleDateString('en-US',options)
+  res.render("list",{kindOfDay:day,listData:lists})
 });
-
+app.post("/",(req, res)=>{
+   let list = req.body.todo
+   lists.push(list)
+  res.redirect("/")
+})
 app.listen(8000, (req, res) => {
   console.log("App is running on the port 8000");
 });
